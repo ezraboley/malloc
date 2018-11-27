abca5079ddd5ac6f2fe83057c72e0f5630cf3ac4
DillonOLeary <doleary@wisc.edu>
2018-11-26 22:06:45 -0600

    Update rb_tree.h

--- rb_tree.h
+++ rb_tree.h
@@ -1,2 +1,28 @@
 /**
- *
+ * This is a self balancing tree, using the red black interface
+ */
+typedef struct {
+  
+} Node
+
+typedef struct {
+
+} Data
+
+/**
+ * Add a node to the tree
+ */
+void add(Node * node);
+
+
+
+/**
+ * look up a value
+ */
+Data * look_up(void * key);
+
+
+/**
+ * key->value lookup
+ */
+Data * remove(void * key);
