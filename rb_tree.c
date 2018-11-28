b5678285945c376de63b9f221e91433ad8457b24
Ezra Boley <eboley@wisc.edu>
2018-11-28 13:55:44 -0600

    Adding the remove command

---
+++ rb_tree.c
@@ -0,0 +1,56 @@
+#include "include/rb_tree.h"
+
+
+//We go right then left
+Node *get_next_inorder(void *key) {
+    Node *node = look_up_node(key);
+    Node *child = node->r_child;
+    while (child != NULL) {
+        child = child->l_child;
+    }
+    return child;
+}
+
+// What is our key?
+Data * remove(void *key) {
+    Node *node = look_up_node(key);
+    if (node->r_child == NULL && node->l_child == NULL) {
+        // No Children
+        
+    
+    } else if (node->r_child != NULL && node->l_child != NULL) {
+        // Two children
+        Node *replacement = get_next_inorder(key);
+        
+        Node *parent = node->parent;
+        // root
+        if (parent->r_child == parent->l_child) {
+            parent->r_child = replacement;
+        } else {
+            if (parent->r_child == node) {
+                // replace here
+                parent->r_child = replacement;
+            } else {
+                parent->l_child = replacement;
+                
+            }
+        }
+    
+    } else {
+        // One child
+        if (node->r_child != NULL) {
+            node = node->
+        } else {
+            
+        }
+    }
+
+        
+}
+
+
+Data * look_up_data(void *key) {
+    Node *node = look_up_node(key);
+    return Node->data;
+}
+
--- safe_malloc.c
+++ safe_malloc.c
@@ -1,7 +1,23 @@
 #include "safe_malloc.h"
+#include <stdlib.h>
+
 
 void *malloc537(size_t size) {
-
+    if (size == 0) {
+        fprintf(stderr, "Allocating memory with size 0, are you sure you wanted to do that?\n");
+    }
+    Data range_data;
+    
+    range_data.addr = malloc(size);
+    Data old_data;
+    if ((old_data = look_up_data(range_data.addr)) != NULL) {
+        if (old_data->freed) {
+            //Reallocate
+        } else {
+            fprintf("Error allocating memory, malloc used an inuse address\n"
+            
+        }
+    }
 }
 
 
