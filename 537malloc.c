8cf9eeac1ae8ed5b459dc4f940450a1882540cb1
Ezra Boley <eboley@wisc.edu>
2018-12-03 18:32:13 -0600

    Work on the range lookup

--- 537malloc.c
+++ 537malloc.c
@@ -2,7 +2,25 @@
 #include "rb_tree.h"
 
 void *malloc537(size_t size) {
+    if (size == 0) {
+        fprintf(stderr, "Allocating memory with size 0 is odd, are you sure?\n"); 
+    }
+    
+    KeyVals range_data;
+    range_data.key = malloc(size);
+    range_data.len = size;
 
+    KeyVals old_data;
+    if ((old_data = lookup_data(range_data.key)) != NULL) {
+        if (old_data.free) {
+            lookup_range();       
+        } else {
+            fprintf(stderr, "Can't malloc an already in use address\n Quitting...\n");
+            exit(1);
+        }
+    } else {
+        insert_data(range_data);
+    }
 }
 
 
--- test_tree.c
+++ test_tree.c
@@ -48,8 +48,59 @@ void create_node(Node * node, int key, int len, int free) {
     node->left = node->right = node->parent = NULL;
 }
 
-int main() {
+void print_node_list(NodeList *list) {
+    ListNode *node = list->frst_node;
+    int count = 0;
+    while (node != NULL) {
+        printf("NODE NUM: %d\n\t KEY: %p, LEN: %i, FREE: %d\n", 
+                count++, node->payload->info.key, node->payload->info.len, node->payload->info.free);
+        node = node->nxt_node; 
+    }
+}
 
+void test_2() {
+    Node * node1 = malloc(sizeof(Node));
+    create_node(node1, 10, 9, 0);
+    Node * root = insert_node(NULL, node1);
+
+    Node * node2 = malloc(sizeof(Node));
+    create_node(node2, 20, 9, 0);
+    root = insert_node(root, node2);
+
+    Node * node3 = malloc(sizeof(Node));
+    create_node(node3, 30, 20, 0);
+    root = insert_node(root, node3);
+
+    Node * node4 = malloc(sizeof(Node));
+    create_node(node4, 40, 9, 0);
+    root = insert_node(root, node4);
+
+    Node * node5 = malloc(sizeof(Node));
+    create_node(node5, 50, 9, 0);
+    root = insert_node(root, node5);
+
+    Node * node6 = malloc(sizeof(Node));
+    create_node(node6, 60, 9, 0);
+    root = insert_node(root, node6);
+
+    Node * node7 = malloc(sizeof(Node));
+    create_node(node7, 70, 9, 0);
+    root = insert_node(root, node7);
+
+    Node * node8 = malloc(sizeof(Node));
+    create_node(node8, 80, 9, 0);
+    root = insert_node(root, node8);
+
+    set_root(root);
+
+    NodeList *list = lookup_range((void *)(long)42, 1);
+    printf("done with look_up\n");
+    print_node_list(list); 
+     
+
+}
+
+void test_1() {
     Node * node1 = malloc(sizeof(Node));
     create_node(node1, 10, 1, 0);
     Node * root = insert_node(NULL, node1);
@@ -104,6 +155,11 @@ int main() {
 
 
     print_tree(root, 1);
+}
 
+int main() {
+    //test_1();
+    test_2();
+    
     return 0;
 }
